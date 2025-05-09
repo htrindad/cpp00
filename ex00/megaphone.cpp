c3f6342b03c5a75815269da341ffba8d225a26ab
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-05-09 17:00:37 +0100

    fc

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 16:49:35 by htrindad          #+#    #+#             */
+/*   Updated: 2025/05/09 16:52:18 by htrindad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		for (int i = 1; i < ac; i++)
+			for (int j = 0; av[i][j]; j++)
+				av[i][j] = toupper(av[i][j]);
+		for (int i = 1; i < ac-1; i++)
+			std::cout << av[i] << ' ';
+		std::cout << av[ac-1] << '\n';
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+}
