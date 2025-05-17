b6dfd0e7f8f4420757ea1a7db1dda848c9554457
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-05-17 18:26:17 +0100

    fixes

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 20:28:18 by htrindad          #+#    #+#             */
-/*   Updated: 2025/05/17 18:15:19 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/05/17 18:25:24 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,40 +29,33 @@ void Account::makeDeposit(int deposit)
 {
 	_amount += deposit;
 	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-	if (withdrawal > _amount)
+	if (withdrawal > _amount || withdrawal > _totalAmount)
 		return false;
 	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
 	return true;
 }
 
-int Account::checkAmount() const
-{
-	return _amount;
-}
+int Account::checkAmount() const { return _amount; }
 
-static int Account::getTotalAmount()
-{
-	return _amount;
-}
+static int Account::getTotalAmount() { return _totalAmount; }
 
-static int Account::getNbDeposits()
-{
-	return _nbDeposits;
-}
+static int Account::getNbDeposits() { return _nbDeposits; }
 
-static int Account::getNbAccounts()
-{
-	return _accountIndex;
-}
+static int Account::getNbAccounts() { return _accountIndex; }
+
+static void Account::_displayTimestamp() {}
 
 static void Account::displayAccountsInfos()
 {
 	std::cout
 		<< "Account Number: " << getNbAccounts() << '\n'
-		<< "Total Amount: " << getTotalAmount() << '\n'
+		<< "Total Amount: " << checkAmount() << '\n'
 		<< "Number of Withdrawals: " << getNbDeposits() << '\n';
 }
